b5ba40d53c077a1bdc3d2cb54296503982e2f28a
moguille <moguille@student.42madrid.com>
2025-03-09 16:05:18 +0100

    ex00

--- ex00/ft_strcpy.c
+++ ex00/ft_strcpy.c
@@ -6,41 +6,38 @@
 /*   By: moguille <moguille@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 14:22:12 by moguille          #+#    #+#             */
-/*   Updated: 2025/03/02 23:20:09 by moguille         ###   ########.fr       */
+/*   Updated: 2025/03/09 16:04:02 by moguille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strcpy(char *dest, char *src)
 {
-	while (*src != '\0')
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		dest[i] = src[i];
+		i++;
 	}
-	*dest = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
 
 /*#include <string.h>
-#include <stdio.h>
+#include i<stdio.h>
 
 int	main(void)
 {
-	char	*src;
 	char	*dest;
-
-	char	a[]= "negrita";
+	char	src[]= "negrita";
 	char	b[]= "amarilla";
 
-	src = &a[0];
 	dest = &b[0];
-
-	printf("la cadena origen: %s\n", src);
-	printf("la cadena destino: %s\n", dest);
-	//strcpy(dest, src);
-	ft_strcpy(dest, src);
-	printf("la cadena origen: %s\n", src);
-	printf("la cadena destino: %s\n", dest);
+	printf("origen: %s\n", src);
+	printf("destino: %s\t%p\n", dest, dest);
+	//printf("%p\n", strcpy(dest, src));
+	printf("%p\n", ft_strcpy(dest, src));
+	printf("destino: %s\n", dest);
 	return (0);
 }*/
